71599292ebcc744b8f07a7c94b7e6769efc4110f
Jeeva1628 <110800795+Jeeva1628@users.noreply.github.com>
2024-07-30 10:28:47 +0530

    Update shiftgame.java

--- shiftgame.java
+++ shiftgame.java
@@ -1,12 +1,17 @@
+import java.lang.*;
 import java.util.*;
-class shiftgame {
+class HelloWorld {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the no of persons:");
         int person = sc.nextInt();
         System.out.println("enter the no of shifts:");
         int shift=sc.nextInt();
-        int rem=person%shift;
-        System.out.println("person "+rem+" has the note now");
+        System.out.println("who has the note now");
+        int start=sc.nextInt();
+        int dif=start-1;
+        int rem=(shift%person)+dif;
+        int res=rem%person;
+        System.out.println("person "+(res+1)+" has the note now");
     }
 }
