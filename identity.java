99a942700fbd4ece489df3fd6d57e3279221f7cb
Jeeva1628 <110800795+Jeeva1628@users.noreply.github.com>
2024-08-01 10:58:56 +0530

    check identity matrix

---
+++ identity.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.util.Arrays;
+class identity {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size  of row and col:");
+        int row = sc.nextInt();
+        int col= sc.nextInt();
+        int temp1=0,temp2=0;
+        int a[][]= new int[row][col];
+        
+        System.out.println("enter the elements:");
+        
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                a[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0; i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(i==j &&  a[i][j]==1 )
+                {
+                        temp1++;
+                }
+                    if(i!=j && a[i][j]==0)
+                    {
+                        temp2++;
+                    }
+                
+                }
+            }
+            if(temp1>0 && temp2>0)
+            {
+                System.out.println("the matrix is an identity matrix");
+            }
+            else
+            {
+             System.out.println("the matrix is not an identity matrix");   
+            }
+     }
+}
