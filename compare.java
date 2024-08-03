fe93fe23922b567c1dcff1d421df6eb1e240c8c7
Jeeva1628 <110800795+Jeeva1628@users.noreply.github.com>
2024-08-03 09:29:53 +0530

    compare string char

---
+++ compare.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.String;
+class result
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=new String("hello");
+        char ch[]={'h','e','l','l','o'};
+        String s=new String(ch);
+        System.out.println(str.equals(s));
+    }
+}
